1498fd102ba0582beb8dbbec3a2baaf1c3a69d32
KonJud <deveuudes@gmail.com>
2024-01-18 15:55:23 +0100

    encore une correction dans la page about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function About() {
   return (
     <>
-        <div className="max-w-6xl" mx-auto p-3 space-y-4>
-            <h1 className="text-2xl" font-medium text-amber-600>
+        <div className="max-w-6xl mx-auto p-3 space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">
                 About
             </h1>
             <p>
